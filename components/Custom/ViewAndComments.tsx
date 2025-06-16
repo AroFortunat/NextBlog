f93b4cfc2b29f7796636ed11f1f31e4c595da1cd
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-06-16 17:20:26 +0300

    refactor: change ViewAndComments component from function to arrow function syntax for consistency

--- components/Custom/ViewAndComments.tsx
+++ components/Custom/ViewAndComments.tsx
@@ -1,12 +1,11 @@
 import { Post } from "@/types";
 import { Eye, MessageCircle } from "lucide-react";
 
-export function ViewAndComments(
+export const  ViewAndComments = (
     {
         data,
         iconCommentSize,
         iconViewSize
-
     }
         :
         {
@@ -14,27 +13,25 @@ export function ViewAndComments(
             iconCommentSize: number;
             iconViewSize: number
         }
-) {
-    return (
-        <div className="flex gap-2">
-            <div className="flex items-center gap-1">
-                <MessageCircle
-                    size={iconCommentSize}
-                    className="text-slate-500" />
-                <p
-                    className="text-slate-500">
-                    {data.nbComments}
-                </p>
-            </div>
-            <div className="flex items-center gap-1">
-                <Eye
-                    size={iconViewSize}
-                    className="text-slate-500" />
-                <p
-                    className="text-slate-500">
-                    {data.nbViews}
-                </p>
-            </div>
+) => (
+    <div className="flex gap-2">
+        <div className="flex items-center gap-1">
+            <MessageCircle
+                size={iconCommentSize}
+                className="text-slate-500" />
+            <p
+                className="text-slate-500">
+                {data.nbComments}
+            </p>
         </div>
-    );
-}
+        <div className="flex items-center gap-1">
+            <Eye
+                size={iconViewSize}
+                className="text-slate-500" />
+            <p
+                className="text-slate-500">
+                {data.nbViews}
+            </p>
+        </div>
+    </div>
+)
