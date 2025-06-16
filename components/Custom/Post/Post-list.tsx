90fb03b354028e19cd140627345eff316d669ed3
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-06-16 14:57:07 +0300

    feat: enhance PostCard component with title, category badge, and comment/view counters

--- components/Custom/Post/Post-list.tsx
+++ components/Custom/Post/Post-list.tsx
@@ -6,7 +6,7 @@ export const Postlist = (
 ) => {
   return (
     <div
-    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
     >
       {posts.map((post)=>(
         <PostCard key={post.id} data={post}/>
--- components/Custom/Post/PostCard.tsx
+++ components/Custom/Post/PostCard.tsx
@@ -1,5 +1,7 @@
+import { Badge } from "@/components/ui/badge"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Post } from "@/types"
+import { Eye, MessageCircle } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -9,7 +11,7 @@ export const PostCard = (
     return (
         <div>
             <Link href={`/posts/${data.slug}`}>
-                <Card className="flex flex-col justify-between rounded-lg border-2">
+                <Card className="flex flex-col justify-between rounded-lg border-2 h-[100%]">
                     <CardHeader>
                         <div className="aspect-square relative">
                             <Image
@@ -18,12 +20,42 @@ export const PostCard = (
                                 src={data.image ?? 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D'}
                                 width={630}
                                 height={630}
-                                // quality={80}
+                            // quality={80}
                             />
                         </div>
+                        <p className="font-semibold text-lg mt-3">{data.title}</p>
                     </CardHeader>
-                    <CardContent></CardContent>
-                    <CardFooter></CardFooter>
+                    <CardContent>
+                        <Badge variant={'outline'}>
+                            {data.category}
+                        </Badge>
+                    </CardContent>
+                    <CardFooter>
+                        <div className="flex gap-2">
+                            <div className="flex items-center gap-1">
+                                <MessageCircle
+                                    size={20}
+                                    className="text-slate-500"
+                                />
+                                <p
+                                    className="text-slate-500"
+                                >
+                                    {data.nbComments}
+                                </p>
+                            </div>
+                            <div className="flex items-center gap-1">
+                                <Eye
+                                    size={20}
+                                    className="text-slate-500"
+                                />
+                                <p
+                                    className="text-slate-500"
+                                >
+                                    {data.nbViews}
+                                </p>
+                            </div>
+                        </div>
+                    </CardFooter>
                 </Card>
             </Link>
         </div>
