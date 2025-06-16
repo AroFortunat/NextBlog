9f0a1aafcabeed621ad71798c0397fabe2805d4e
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-06-16 13:00:56 +0300

    fix: add relative positioning to PostCard image container for better layout

--- components/Custom/Post/PostCard.tsx
+++ components/Custom/Post/PostCard.tsx
@@ -11,9 +11,9 @@ export const PostCard = (
             <Link href={`/posts/${data.slug}`}>
                 <Card className="flex flex-col justify-between rounded-lg border-2">
                     <CardHeader>
-                        <div className="aspect-square">
+                        <div className="aspect-square relative">
                             <Image
-                                className="aspect-square object-cover transition-all"
+                                className="aspect-square object-cover transition-all duration-300 hover:scale-110"
                                 alt="image d'un post"
                                 src={data.image ?? 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dGVjaHxlbnwwfHwwfHx8MA%3D%3D'}
                                 width={630}
