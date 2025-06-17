e25a66ece3bd6c317c8471936a6ad2bd8049c282
AroFortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-06-17 16:00:30 +0300

    feat: update Prisma client connection and API route for fetching posts by slug

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,25 +1,24 @@
-import { Post } from "@/types";
+import { prisma } from "@/lib/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-const POST: Post = {
-    id: 1,
-    category: "React",
-    title: "React State Management: Choosing the Right Solution",
-    image: "https://images.unsplash.com/photo-1633356122544-f134324a6cee",
-    caption:
-      "Explore different state management solutions in React and choose the one that fits your needs.",
-    date: "2023-01-15",
-    minutesToRead: 10,
-    author: "John ReactDev",
-    nbViews: 25,
-    nbComments: 8,
-    slug: "react-state-management-choosing-right-solution",
-    content:'Hello'
-  }
 export const GET = async (
   req: NextRequest,
   { params }: { params: { slug: string } }
 ) => {
-  return NextResponse.json(POST,{status:200});
+  try {
+    const { slug } = await params;
+
+    const postBySlug = await prisma.post.findUnique({
+      where: { slug },
+    });
+
+    return NextResponse.json(postBySlug, { status: 200 });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Error something wrong" },
+      { status: 200 }
+    );
+  }
 };
- 
